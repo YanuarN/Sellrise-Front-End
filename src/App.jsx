9da8cb04feb49caa108c7ac9f88efaf070ee4aa2
Yanuar <yanuaralfi@gmail.com>
2026-03-06 13:31:48 +0700

    feat: add LandingPage component with assets and layout for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './layout';
 import LeadManagement from './pages/LeadManagement/LeadManagement';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<LeadManagement />} />
-          <Route path="/crm" element={<LeadManagement />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <MainLayout>
+              <LeadManagement />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/crm"
+          element={
+            <MainLayout>
+              <LeadManagement />
+            </MainLayout>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
